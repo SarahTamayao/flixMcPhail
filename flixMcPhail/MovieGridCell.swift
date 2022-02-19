195fe1e6af4132d6be8eca5d733d0ddcdc7da1e5
W-McPhail <william.m.mcphail@gmail.com>
2022-02-18 22:24:06 -0500

    pt2
    no bonuses yet

---
+++ flixMcPhail/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixMcPhail
+//
+//  Created by William McPhail on 2/18/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
